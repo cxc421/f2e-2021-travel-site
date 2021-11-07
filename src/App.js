94f6f2dfebee8f6a02ab8e608c95fdac2ac604c7
Chris Chuang <chrischuang0421@gmail.com>
2021-11-08 00:52:32 +0800

    Add header component for tablet layout

--- src/App.js
+++ src/App.js
@@ -3,6 +3,17 @@ function App() {
   return (
     <>
       <Header />
+      <main
+        style={{
+          width: "100%",
+          height: "1000px",
+          overflow: "auto",
+          position: "relative",
+          zIndex: 1,
+        }}
+      >
+        <h1 style={{ color: "black", margin: 0 }}>Test</h1>
+      </main>
     </>
   );
 }
--- src/components/header/header.component.js
+++ src/components/header/header.component.js
@@ -1,38 +1,50 @@
-import logoSrc from "./logo.png";
-import greenCircleSrc from "./green-circle-circle.png";
-import yellowCircleSrc from "./yellow-rectangle-circle.png";
-import redCircleSrc from "./red-triangle-circle.png";
+import logoSrc from "./images/logo.png";
+import greenCircleSrc from "./images/green-circle-circle.png";
+import yellowCircleSrc from "./images/yellow-rectangle-circle.png";
+import redCircleSrc from "./images/red-triangle-circle.png";
+import cardShadowSrc from "./images/card-shadow.png";
 import styles from "./header.module.css";
 
 function Header() {
   return (
-    <header className={styles.header}>
-      <a className={styles.logo} href="/">
-        <img src={logoSrc} alt="logo" />
-      </a>
-      <nav className={styles.navbar}>
-        <ul>
-          <li>
-            <a className={styles.redAnchor} href="/travel">
-              <img src={redCircleSrc} alt="Red circle" />
-              <span>台灣景點</span>
-            </a>
-          </li>
-          <li>
-            <a className={styles.yellowAnchor} href="/travel">
-              <img src={yellowCircleSrc} alt="Yellow circle" />
-              <span>美食住宿</span>
-            </a>
-          </li>
-          <li>
-            <a className={styles.greenAnchor} href="/travel">
-              <img src={greenCircleSrc} alt="Green circle" />
-              <span>景點交通</span>
-            </a>
-          </li>
-        </ul>
-      </nav>
-    </header>
+    <>
+      <div className={styles.placeHolder} />
+      <header className={styles.header}>
+        <div className={styles.headerContent}>
+          <a className={styles.logo} href="/">
+            <img src={logoSrc} alt="logo" />
+          </a>
+          <nav className={styles.navbar}>
+            <ul>
+              <li>
+                <a className={styles.redText} href="/travel">
+                  <img src={redCircleSrc} alt="Red circle" />
+                  <span>台灣景點</span>
+                </a>
+              </li>
+              <li>
+                <a className={styles.yellowText} href="/travel">
+                  <img src={yellowCircleSrc} alt="Yellow circle" />
+                  <span>美食住宿</span>
+                </a>
+              </li>
+              <li>
+                <a className={styles.greenText} href="/travel">
+                  <img src={greenCircleSrc} alt="Green circle" />
+                  <span>景點交通</span>
+                </a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+        <div className={styles.headerContentSmall}></div>
+      </header>
+      <img
+        src={cardShadowSrc}
+        className={styles.cardShadow}
+        alt="card shadow"
+      />
+    </>
   );
 }
 
